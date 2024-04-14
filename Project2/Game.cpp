37405c2c68a826d3f35d5080283a2197d2eab057
AhmedsElysium <ahmadyassermo@gmail.com>
2024-04-14 07:12:23 +0200

    Revert "Fixed some stuff"
    This reverts commit ac225235a12c24cc6b6f0d5846d8eabf2983c514.

--- Project2/Game.cpp
+++ Project2/Game.cpp
@@ -1,15 +1,1 @@
-#include "dataStructures.h"
-
-
-int main() {
-
-	Stack<int> intStack;
-	int x = 0;
-	intStack.push(x);
-	x = 2;
-	intStack.push(x);
-	x = 5;
-	intStack.push(x);
-	intStack.print();
-
-}
+#include "Game.h"
--- Project2/dataStructures.h
+++ Project2/dataStructures.h
@@ -8,7 +8,6 @@ template <typename T>
 struct Node {
 	T data;
 	Node<T>* next;
-	Node(T data, Node<T>* next);
 };
 
 
@@ -32,7 +31,7 @@ template <typename T>
 class Stack{
 private:
 	Node<T>* top;
-	void print(Node<T>* node);
+	void print(Node<T> node);
 public:
 	Stack();
 	bool push(T& Data);
@@ -47,9 +46,7 @@ class Queue{
 private:
 	Node<T>* head;
 	Node<T>* rear;
-	void print(Node<T> node);
 public:
-	Queue();
 	bool enqueue(T& Data);
 	bool dequeue(T& topEntry);
 	void print();
@@ -89,111 +86,3 @@ public:
 	void print();
 
 };
-
-
-//Node
-template <typename T>
-Node<T>::Node(T data, Node<T>* next) :data(data), next(next) {};
-
-
-//Stack code
-template <typename T>
-Stack<T>::Stack() {
-	top = nullptr;
-};
-
-template <typename T>
-bool Stack<T>::isEmpty() {
-	return top;
-}
-
-template<typename T>
-bool Stack<T>::push(T& Data) {
-	top = new Node<T>(Data, top);
-	return true;
-}
-
-template<typename T>
-bool Stack<T>::pop(T& Data) {
-	if (isEmpty()) { return false; };
-	Data = top->data;
-	Node<T>* temp;
-	temp = top;
-	top = top->next;
-	delete temp;
-	return true;
-}
-
-template<typename T>
-bool Stack<T>::peek(T& Data) {
-	if (isEmpty()) { return false; };
-	Data = top->data;
-	return true;
-}
-
-template<typename T>
-void Stack<T>::print(Node<T>* node) {
-	if (!node) { return; };
-	cout << node->data << endl;
-	return print(node->next);
-}
-
-template<typename T>
-void Stack<T>::print() {
-	print(top);
-}
-
-
-//Queue code
-template<typename T>
-Queue<T>::Queue() { rear = head = nullptr; }
-
-template<typename T>
-bool Queue<T>::isEmpty() {
-	return head;
-}
-
-template<typename T>
-bool Queue<T>::enqueue(T& Data) {
-	if (isEmpty()) {
-		head = new Node<T>(Data, nullptr);
-		rear = head;
-	}
-	else {
-		rear->next = new Node<T>(Data, nullptr);
-		rear = rear->next;
-	};
-	return true;
-}
-
-template<typename T>
-bool Queue<T>::dequeue(T& topEntry) {
-	if (isEmpty()) {
-		return false;
-	};
-
-	topEntry = head->data;
-	head = head->next;
-	return true;
-}
-
-template<typename T>
-bool Queue<T>::peek(T& topEntry) {
-	if (isEmpty()) {
-		return false;
-	};
-	topEntry = head->data;
-	return true;
-}
-
-template<typename T>
-void Queue<T>::print(Node<T> node) {
-	if (!node) return;
-	cout << node->data << endl;
-	return print(node->next);
-}
-
-template<typename T>
-void Queue<T>::print() {
-	print(head);
-}
